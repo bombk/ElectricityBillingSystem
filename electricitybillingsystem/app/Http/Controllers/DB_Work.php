e2cab8e19ed631acd7feecfec0d61806be9b4ebf
Bom Bdr BK <bomncit@gmail.com>
2021-03-27 12:56:57 +0545

    slect fuction change

--- electricitybillingsystem/app/Http/Controllers/DB_Work.php
+++ electricitybillingsystem/app/Http/Controllers/DB_Work.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\DB;
 class DB_Work extends Controller
 {
     function select(){
-        //$result=DB::table('5amp')->select('minAmt')->get();
-        $result=DB::table('5amp')->select('minAmt')->where('kw/hr','0-10')->get();
+        $result=DB::table('5amp')->select('minAmt','energyRate')->get();
+       // $result=DB::table('5amp')->select('minAmt')->where('kw/hr','0-10')->get();
         
         echo '<pre>';
         print_r($result);
