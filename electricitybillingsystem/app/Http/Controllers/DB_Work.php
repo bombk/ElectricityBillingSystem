168dedd9152b4a36015196c955672986e3209cc1
Bom Bdr BK <bomncit@gmail.com>
2021-03-27 12:53:44 +0545

    make controller DB_work

--- electricitybillingsystem/app/Http/Controllers/DB_Work.php
+++ electricitybillingsystem/app/Http/Controllers/DB_Work.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DB_Work extends Controller
 {
-    //
+    function select(){
+        //$result=DB::table('5amp')->select('minAmt')->get();
+        $result=DB::table('5amp')->select('minAmt')->where('kw/hr','0-10')->get();
+        
+        echo '<pre>';
+        print_r($result);
+
+   }
 }
--- electricitybillingsystem/routes/web.php
+++ electricitybillingsystem/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DB_Work;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get("select",[DB_Work::class,'select']);
