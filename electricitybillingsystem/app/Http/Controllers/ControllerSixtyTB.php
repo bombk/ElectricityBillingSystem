eeb85a6a9a3e7aff35528da2a599460fdeed775a
Bom Bdr BK <bomncit@gmail.com>
2021-04-05 17:54:39 +0545

    file added up to calculations breakdown

--- electricitybillingsystem/app/Http/Controllers/ControllerSixtyTB.php
+++ electricitybillingsystem/app/Http/Controllers/ControllerSixtyTB.php
@@ -18,105 +18,180 @@ class ControllerSixtyTB extends Controller
 
  
    public function calculate(Request $req){
-     
-    $unit= $req->unit;
 
-    if($unit<=10){
-      $qmlv=0;
+    $minAmt=Amp::find(1)->getAmpDetails->pluck('minamt');
 
-      $minAmt=Amp::find(4)->getAmpDetails->pluck('minamt');
+    $energyRate=Amp::find(1)->getAmpDetails->pluck('energyrate');
 
-      $energyRate=Amp::find(4)->getAmpDetails->pluck('energyrate');
-     
-    
-     $calc=$minAmt[0]+$qmlv+$unit*$energyRate[0];
-   
-
-     return "Total Amount is Rs. ".$calc;
-
-    }
-    else if($unit > 10 && $unit <= 20){
-      $qmlv=0;
-      $minAmt=Amp::find(4)->getAmpDetails->pluck('minamt');
-
-      $energyRate=Amp::find(4)->getAmpDetails->pluck('energyrate');
-     
-    
-     $calc=$minAmt[1]+$qmlv+$unit*$energyRate[1];
-      return "Total Amount is Rs. ".$calc;
-
-    }
-    else if($unit > 20 && $unit <= 30){
-      $qmlv=60;
-      $minAmt=Amp::find(4)->getAmpDetails->pluck('minamt');
-
-      $energyRate=Amp::find(4)->getAmpDetails->pluck('energyrate');
-     
-     $calc=$minAmt[2]+$qmlv+($unit-20)*$energyRate[2];
-      return "Total Amount is Rs. ".$calc;
-
-    }
-    else if($unit > 30 && $unit <= 50){
-      $qmlv=125;
-      $minAmt=Amp::find(4)->getAmpDetails->pluck('minamt');
-
-      $energyRate=Amp::find(4)->getAmpDetails->pluck('energyrate');
-     
-      $calc=$minAmt[3]+$qmlv+($unit-30)*$energyRate[3];
-      return "Total Amount is Rs. ".$calc;
-
-    }
-    else if($unit > 50 && $unit <= 100){
-      $qmlv=285;
-      $minAmt=Amp::find(4)->getAmpDetails->pluck('minamt');
-
-      $energyRate=Amp::find(4)->getAmpDetails->pluck('energyrate');
-     
-      $calc=$minAmt[4]+$qmlv+($unit-50)*$energyRate[4];
-      return "Total Amount is Rs. ".$calc;
-
-    }
-    else if($unit > 100 && $unit <= 150){
-      $qmlv=760;
-      $minAmt=Amp::find(4)->getAmpDetails->pluck('minamt');
-
-      $energyRate=Amp::find(4)->getAmpDetails->pluck('energyrate');
-     
-      $calc=$minAmt[5]+$qmlv+($unit-100)*$energyRate[5];
-      return "Total Amount is Rs. ".$calc;
-
-    }
-    else if($unit > 150 && $unit <= 250){
-      $qmlv=1235;
-      $minAmt=Amp::find(4)->getAmpDetails->pluck('minamt');
-
-      $energyRate=Amp::find(4)->getAmpDetails->pluck('energyrate');
-     
-      $calc=$minAmt[6]+$qmlv+($unit-150)*$energyRate[6];
-      return "Total Amount is Rs. ".$calc;
-
-    }
-    else if($unit > 250 && $unit <= 400){
-      $qmlv=2235;
-      $minAmt=Amp::find(4)->getAmpDetails->pluck('minamt');
-
-      $energyRate=Amp::find(4)->getAmpDetails->pluck('energyrate');
-     
-      $calc=$minAmt[7]+$qmlv+($unit-250)*$energyRate[7];
-      return "Total Amount is Rs. ".$calc;
-
-    }
-    
-    else{
-      $qmlv=4035;
-      $minAmt=Amp::find(4)->getAmpDetails->pluck('minamt');
-
-      $energyRate=Amp::find(4)->getAmpDetails->pluck('energyrate');
-
-      $calc=$minAmt[8]+$qmlv+($unit-400)*$energyRate[8];
-      return "Total Amount is Rs. ".$calc;
-    }
+        $unit= $req->unit;
 
 
-    }
+        if($unit <=10){
+          
+       
+        echo "Calculation! <br><br> Total Unit ".$unit;
+        $calc=$minAmt[0]+$unit*$energyRate[0];
+         echo "<br><br>Total Amount is Rs. ".$calc;
+
+        }
+        else if($unit >10 && $unit <= 20){
+            $unitCost=$unit*$energyRate[1];
+          echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+         
+          $calc=$minAmt[1]+$unitCost;
+
+          return "Total Amount is Rs. ".$calc;
+
+        }
+        else if($unit > 20 && $unit <= 30){
+        
+          $unitCost=20*$energyRate[1];
+
+          $unitCost1=($unit-20)*$energyRate[2];
+          echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+          echo "21-30 Unit Cost Rs. ".$unitCost1."<br><br>";
+
+          $calc=$minAmt[2]+$unitCost+$unitCost1;
+          return "Total Amount is Rs. ".$calc;
+
+        }
+        else if($unit > 30 && $unit <= 50){
+          $unitCost=20*$energyRate[1];
+
+          $unitCost1=10*$energyRate[2];
+
+          $unitCost2=($unit-30)*$energyRate[3];
+
+
+          echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+          echo "21-30 Unit Cost Rs. ".$unitCost1."<br><br>";
+          echo "31-50 Unit Cost Rs. ".$unitCost2."<br><br>";
+         
+          $calc=$minAmt[3]+$unitCost1+$unitCost1+$unitCost2;
+          return "Total Amount is Rs. ".$calc;
+
+        }
+        else if($unit > 50 && $unit <= 100){
+          $unitCost=20*$energyRate[1];
+
+          $unitCost1=10*$energyRate[2];
+
+          $unitCost2=20*$energyRate[3];
+
+          $unitCost3=($unit-50)*$energyRate[4];
+
+
+          echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+          echo "21-30 Unit Cost Rs. ".$unitCost1."<br><br>";
+          echo "31-50 Unit Cost Rs. ".$unitCost2."<br><br>";
+          echo "51-100 Unit Cost Rs. ".$unitCost3."<br><br>";
+         
+          $calc=$minAmt[3]+$unitCost1+$unitCost1+$unitCost2+$unitCost3;
+          return "Total Amount is Rs. ".$calc;
+
+        }
+        else if($unit > 100 && $unit <= 150){
+          $unitCost=20*$energyRate[1];
+
+          $unitCost1=10*$energyRate[2];
+
+          $unitCost2=20*$energyRate[3];
+
+          $unitCost3=50*$energyRate[4];
+
+          $unitCost4=($unit-100)*$energyRate[5];
+
+
+          echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+          echo "21-30 Unit Cost Rs. ".$unitCost1."<br><br>";
+          echo "31-50 Unit Cost Rs. ".$unitCost2."<br><br>";
+          echo "51-100 Unit Cost Rs. ".$unitCost3."<br><br>";
+          echo "101-150 Unit Cost Rs. ".$unitCost4."<br><br>";
+         
+          $calc=$minAmt[3]+$unitCost1+$unitCost1+$unitCost2+$unitCost3+$unitCost4;
+          return "Total Amount is Rs. ".$calc;
+
+        }
+        else if($unit > 150 && $unit <= 250){
+          $unitCost=20*$energyRate[1];
+
+          $unitCost1=10*$energyRate[2];
+
+          $unitCost2=20*$energyRate[3];
+
+          $unitCost3=50*$energyRate[4];
+          
+          $unitCost4=50*$energyRate[5];
+          $unitCost5=($unit-150)*$energyRate[6];
+
+
+          echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+          echo "21-30 Unit Cost Rs. ".$unitCost1."<br><br>";
+          echo "31-50 Unit Cost Rs. ".$unitCost2."<br><br>";
+          echo "51-100 Unit Cost Rs. ".$unitCost3."<br><br>";
+          echo "101-150 Unit Cost Rs. ".$unitCost4."<br><br>";
+          echo "151-250 Unit Cost Rs. ".$unitCost5."<br><br>";
+         
+          $calc=$minAmt[3]+$unitCost1+$unitCost1+$unitCost2+$unitCost3+$unitCost4+$unitCost5;
+          return "Total Amount is Rs. ".$calc;
+
+        }
+        else if($unit > 250 && $unit <= 400){
+          $unitCost=20*$energyRate[1];
+
+          $unitCost1=10*$energyRate[2];
+
+          $unitCost2=20*$energyRate[3];
+
+          $unitCost3=50*$energyRate[4];
+          
+          $unitCost4=50*$energyRate[5];
+
+          $unitCost5=100*$energyRate[6];
+
+          $unitCost6=($unit-250)*$energyRate[7];
+
+          echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+          echo "21-30 Unit Cost Rs. ".$unitCost1."<br><br>";
+          echo "31-50 Unit Cost Rs. ".$unitCost2."<br><br>";
+          echo "51-100 Unit Cost Rs. ".$unitCost3."<br><br>";
+          echo "101-150 Unit Cost Rs. ".$unitCost4."<br><br>";
+          echo "151-250 Unit Cost Rs. ".$unitCost5."<br><br>";
+          echo "251-400 Unit Cost Rs. ".$unitCost6."<br><br>";
+         
+          $calc=$minAmt[3]+$unitCost1+$unitCost1+$unitCost2+$unitCost3+$unitCost4+$unitCost5+$unitCost6;
+          return "Total Amount is Rs. ".$calc;
+
+        }
+        
+        else{
+          $unitCost=20*$energyRate[1];
+
+          $unitCost1=10*$energyRate[2];
+
+          $unitCost2=20*$energyRate[3];
+
+          $unitCost3=50*$energyRate[4];
+          
+          $unitCost4=50*$energyRate[5];
+
+          $unitCost5=100*$energyRate[6];
+
+          $unitCost6=150*$energyRate[7];
+          $unitCost7=($unit-400)*$energyRate[8];
+
+          echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+          echo "21-30 Unit Cost Rs. ".$unitCost1."<br><br>";
+          echo "31-50 Unit Cost Rs. ".$unitCost2."<br><br>";
+          echo "51-100 Unit Cost Rs. ".$unitCost3."<br><br>";
+          echo "101-150 Unit Cost Rs. ".$unitCost4."<br><br>";
+          echo "151-250 Unit Cost Rs. ".$unitCost5."<br><br>";
+          echo "251-400 Unit Cost Rs. ".$unitCost6."<br><br>";
+          echo "401 Above Unit Cost Rs. ".$unitCost7."<br><br>";
+         
+          $calc=$minAmt[3]+$unitCost1+$unitCost1+$unitCost2+$unitCost3+$unitCost4+$unitCost5+$unitCost6+$unitCost7;
+          return "Total Amount is Rs. ".$calc;
+        }
+
+  }
 }
--- electricitybillingsystem/app/Http/Controllers/DB_Work.php
+++ electricitybillingsystem/app/Http/Controllers/DB_Work.php
@@ -54,74 +54,170 @@ class DB_Work extends Controller
           $unit= $req->unit;
 
 
-          if($unit<=10){
-            $qmlv=0;
-
-           $calc=$minAmt[0]+$qmlv+$unit*$energyRate[0];
+          if($unit <=10){
+            
          
-          echo "Total Amount is Rs. ".$calc;
+          echo "Calculation! <br><br> Total Unit ".$unit;
+          $calc=$minAmt[0]+$unit*$energyRate[0];
+           echo "<br><br>Total Amount is Rs. ".$calc;
 
           }
           else if($unit >10 && $unit <= 20){
-            $qmlv=0;
+              $unitCost=$unit*$energyRate[1];
+            echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
            
-  
-           $calc=$minAmt[1]+$qmlv+$unit*$energyRate[1];
+            $calc=$minAmt[1]+$unitCost;
+
             return "Total Amount is Rs. ".$calc;
 
           }
           else if($unit > 20 && $unit <= 30){
-            $qmlv=60;
-           
-           $calc=$minAmt[2]+$qmlv+($unit-20)*$energyRate[2];
+          
+            $unitCost=20*$energyRate[1];
+
+            $unitCost1=($unit-20)*$energyRate[2];
+            echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+            echo "21-30 Unit Cost Rs. ".$unitCost1."<br><br>";
+
+            $calc=$minAmt[2]+$unitCost+$unitCost1;
             return "Total Amount is Rs. ".$calc;
 
           }
           else if($unit > 30 && $unit <= 50){
-            $qmlv=125;
-          
-            $calc=$minAmt[3]+$qmlv+($unit-30)*$energyRate[3];
+            $unitCost=20*$energyRate[1];
+
+            $unitCost1=10*$energyRate[2];
+
+            $unitCost2=($unit-30)*$energyRate[3];
+
+
+            echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+            echo "21-30 Unit Cost Rs. ".$unitCost1."<br><br>";
+            echo "31-50 Unit Cost Rs. ".$unitCost2."<br><br>";
+           
+            $calc=$minAmt[3]+$unitCost1+$unitCost1+$unitCost2;
             return "Total Amount is Rs. ".$calc;
 
           }
           else if($unit > 50 && $unit <= 100){
-            $qmlv=285;
+            $unitCost=20*$energyRate[1];
+
+            $unitCost1=10*$energyRate[2];
+
+            $unitCost2=20*$energyRate[3];
+
+            $unitCost3=($unit-50)*$energyRate[4];
+
+
+            echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+            echo "21-30 Unit Cost Rs. ".$unitCost1."<br><br>";
+            echo "31-50 Unit Cost Rs. ".$unitCost2."<br><br>";
+            echo "51-100 Unit Cost Rs. ".$unitCost3."<br><br>";
            
-           
-            $calc=$minAmt[4]+$qmlv+($unit-50)*$energyRate[4];
+            $calc=$minAmt[3]+$unitCost1+$unitCost1+$unitCost2+$unitCost3;
             return "Total Amount is Rs. ".$calc;
 
           }
           else if($unit > 100 && $unit <= 150){
-            $qmlv=760;
+            $unitCost=20*$energyRate[1];
+
+            $unitCost1=10*$energyRate[2];
+
+            $unitCost2=20*$energyRate[3];
+
+            $unitCost3=50*$energyRate[4];
+
+            $unitCost4=($unit-100)*$energyRate[5];
+
+
+            echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+            echo "21-30 Unit Cost Rs. ".$unitCost1."<br><br>";
+            echo "31-50 Unit Cost Rs. ".$unitCost2."<br><br>";
+            echo "51-100 Unit Cost Rs. ".$unitCost3."<br><br>";
+            echo "101-150 Unit Cost Rs. ".$unitCost4."<br><br>";
            
-           
-            $calc=$minAmt[5]+$qmlv+($unit-100)*$energyRate[5];
+            $calc=$minAmt[3]+$unitCost1+$unitCost1+$unitCost2+$unitCost3+$unitCost4;
             return "Total Amount is Rs. ".$calc;
 
           }
           else if($unit > 150 && $unit <= 250){
-            $qmlv=1235;
+            $unitCost=20*$energyRate[1];
+
+            $unitCost1=10*$energyRate[2];
+
+            $unitCost2=20*$energyRate[3];
+
+            $unitCost3=50*$energyRate[4];
+            
+            $unitCost4=50*$energyRate[5];
+            $unitCost5=($unit-150)*$energyRate[6];
+
+
+            echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+            echo "21-30 Unit Cost Rs. ".$unitCost1."<br><br>";
+            echo "31-50 Unit Cost Rs. ".$unitCost2."<br><br>";
+            echo "51-100 Unit Cost Rs. ".$unitCost3."<br><br>";
+            echo "101-150 Unit Cost Rs. ".$unitCost4."<br><br>";
+            echo "151-250 Unit Cost Rs. ".$unitCost5."<br><br>";
            
-           
-            $calc=$minAmt[6]+$qmlv+($unit-150)*$energyRate[6];
+            $calc=$minAmt[3]+$unitCost1+$unitCost1+$unitCost2+$unitCost3+$unitCost4+$unitCost5;
             return "Total Amount is Rs. ".$calc;
 
           }
           else if($unit > 250 && $unit <= 400){
-            $qmlv=2235;
-          
+            $unitCost=20*$energyRate[1];
+
+            $unitCost1=10*$energyRate[2];
+
+            $unitCost2=20*$energyRate[3];
+
+            $unitCost3=50*$energyRate[4];
+            
+            $unitCost4=50*$energyRate[5];
+
+            $unitCost5=100*$energyRate[6];
+
+            $unitCost6=($unit-250)*$energyRate[7];
+
+            echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+            echo "21-30 Unit Cost Rs. ".$unitCost1."<br><br>";
+            echo "31-50 Unit Cost Rs. ".$unitCost2."<br><br>";
+            echo "51-100 Unit Cost Rs. ".$unitCost3."<br><br>";
+            echo "101-150 Unit Cost Rs. ".$unitCost4."<br><br>";
+            echo "151-250 Unit Cost Rs. ".$unitCost5."<br><br>";
+            echo "251-400 Unit Cost Rs. ".$unitCost6."<br><br>";
            
-            $calc=$minAmt[7]+$qmlv+($unit-250)*$energyRate[7];
+            $calc=$minAmt[3]+$unitCost1+$unitCost1+$unitCost2+$unitCost3+$unitCost4+$unitCost5+$unitCost6;
             return "Total Amount is Rs. ".$calc;
 
           }
           
           else{
-            $qmlv=4035;
-           
+            $unitCost=20*$energyRate[1];
 
-            $calc=$minAmt[8]+$qmlv+($unit-400)*$energyRate[8];
+            $unitCost1=10*$energyRate[2];
+
+            $unitCost2=20*$energyRate[3];
+
+            $unitCost3=50*$energyRate[4];
+            
+            $unitCost4=50*$energyRate[5];
+
+            $unitCost5=100*$energyRate[6];
+
+            $unitCost6=150*$energyRate[7];
+            $unitCost7=($unit-400)*$energyRate[8];
+
+            echo "Calculation! <br><br> Total Unit ".$unit."<br><br>0-20 Unit Cost Rs. ".$unitCost."<br><br>";
+            echo "21-30 Unit Cost Rs. ".$unitCost1."<br><br>";
+            echo "31-50 Unit Cost Rs. ".$unitCost2."<br><br>";
+            echo "51-100 Unit Cost Rs. ".$unitCost3."<br><br>";
+            echo "101-150 Unit Cost Rs. ".$unitCost4."<br><br>";
+            echo "151-250 Unit Cost Rs. ".$unitCost5."<br><br>";
+            echo "251-400 Unit Cost Rs. ".$unitCost6."<br><br>";
+            echo "401 Above Unit Cost Rs. ".$unitCost7."<br><br>";
+           
+            $calc=$minAmt[3]+$unitCost1+$unitCost1+$unitCost2+$unitCost3+$unitCost4+$unitCost5+$unitCost6+$unitCost7;
             return "Total Amount is Rs. ".$calc;
           }
 
