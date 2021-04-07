3b3534d021fe65b0d266d74a033d4aaed21c248a
Bom Bdr BK <bomncit@gmail.com>
2021-04-07 11:44:11 +0545

    edit login file

--- electricitybillingsystem/resources/views/login.blade.php
+++ electricitybillingsystem/resources/views/login.blade.php
@@ -10,8 +10,10 @@
     <h3>Login User</h3>
     <form id="form" class="" action="{{URL::to('logs')}}" method="post">
         @csrf
-        <input type="text" name="name" required placeholder="name" value=""><br><br>
-        <input type="password" name="password" required placeholder="password" value=""><br><br>
+        <label>Name</label><br><br>
+        <input type="text" name="name" required placeholder="Enter name" value=""><br><br>
+        <label>Password</label><br><br>
+        <input type="password" name="password" required placeholder="Enter password" value=""><br><br>
         <button type="submit" name="button">Login</button>
     </form>
     <br><br>
